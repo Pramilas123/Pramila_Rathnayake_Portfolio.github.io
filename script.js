51642c6ddb569bfc253ae64dda26c6e9adcd3e62
Pramilas123 <137532176+Pramilas123@users.noreply.github.com>
2026-03-03 23:02:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+
+window.addEventListener("scroll", function() {
+
+    const reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+});
+
+</script>
