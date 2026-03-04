fe805f0cc0ba6d15ca1bf9e6923b4e00ec519c1c
Pramilas123 <137532176+Pramilas123@users.noreply.github.com>
2026-03-04 13:05:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ window.addEventListener("scroll", function() {
         }
     }
 });
+
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function nextSlide() {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+}
+
+setInterval(nextSlide, 4000);
